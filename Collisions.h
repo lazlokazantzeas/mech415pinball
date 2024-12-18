a741246be03e673df80eb874719694b6643694da
ZachAmyotte <zach.amyotte@gmail.com>
2024-12-17 22:53:48 -0500

    Update Collisions.h

--- Collisions.h
+++ Collisions.h
@@ -4,9 +4,13 @@
 #include "Flipper.h"
 
 //function prototype for boundary (exterior wall) collisions
-void boundaryCollision(Ball& ball, int screenWidth, int screenHeight);
+bool boundaryCollision(Ball& ball, int screenWidth, int screenHeight);
 
-void circleCollisions(Ball& ball, circleStage& circle);
+bool circleCollisions(Ball& ball, circleStage& circle);
 
 // function prototype for collisions with the flippers
 void flipperCollision(Ball& ball, Flipper& flipper, bool pivotAtRight);
+
+void initialCollision(Ball& ball);
+
+void stageCollisions(Ball& ball);
